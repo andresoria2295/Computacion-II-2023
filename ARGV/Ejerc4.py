89c12e2ff0fa39e7d1602eed13bec2fbf3abb519
andresoria2295 <andres.95.za@gmail.com>
2023-03-29 11:25:21 -0300

    Recepción de lectura

--- ARGV/Ejerc4.py
+++ ARGV/Ejerc4.py
@@ -2,28 +2,37 @@
 4- Continuar el ejercicio comenzado en clase para utilizar la salida de bajo nivel.
 python3 Ejerc4.py < prueba.txt
 python3 Ejerc4.py < /etc/services 2 > log (lo manda a archivo log)
-
 '''
 
 #!/usr/bin/python3
 import sys
 import os
+lectura = []
+
+sys.stdout.write("\nLectura de documento: \n")
+sys.stdout.write('\n')
 
 while True:
     leido = (os.read(0,1024)).decode("utf-8")
     if len(leido) < 1024:
         print(leido, end = '')
+        lectura.append(leido)
         break;
     else:
         print(leido, end = '')
+        lectura.append(leido)
 
-lectura = leido.split()
 #os.open("Ejerc4Guardado.txt",os.O_WRITE)
-with open("Ejerc4Guardado.txt", "w") as f:
-    while True:
-        os.write(1,1024)
-        print(lectura, file=f)
+sys.stdout.write("\nLectura de documento volcada a ArchivoLeido.txt")
+sys.stdout.write("\n")
 
+with open("ArchivoLeido.txt", "w") as f:
+    for i in range(len(lectura)):
+        print(lectura[i], file=f)
+    sys.stdout.write('\n')
 
-#with open("errors.log", "w") as f:
-#    print_exc(file=f)
+'''
+new_file = open('Ejerc4Guardado.txt', 'w')
+sys.stdout.write(lectura, file=new_file)
+new_file.close()
+'''
